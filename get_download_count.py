43c7bc7a9046b6e4e9b7a747051ce6d41218d9af
Lars Frogner <lars_frogner@hotmail.com>
2017-03-16 13:23:23 +0100

    Switch newlines from Windows style to Linux

--- get_download_count.py
+++ get_download_count.py
@@ -1,22 +1,23 @@
 import requests
-starts = [0, 0, 0, 0, 0, 0, 40, 0, 88, 63, 36]
+starts = [0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 88, 63, 36]
 tot = 0
 tot_l = 0
-latest = [0, 1, 2, 3, 4]
-ub = [0]
+latest = [0, 1]
 i = 0
 print '\nIndividual download counts:'
 for release in requests.get('https://api.github.com/repos/lars-frogner/astrophotography-lab/releases').json():
+
+
     for release_assets in release['assets']:
     
         d = release_assets['download_count'] + starts[i]
         
         tot += d
         if i in latest: tot_l += d
-        name = release_assets['name'].split('-win-') if not i in ub else release_assets['name'].split('-ubuntu-')
+
+        print '%-34s %d' % (release_assets['name'].replace('.zip', '').replace('.tar.gz', ''), d)
         
-        print '%s: %d' % (name[0].replace('-', ' ') + (' Windows ' if not i in ub else ' Ubuntu ') + name[1].split('.')[0], d)
         i += 1
       
-print '\nDownloads of latest: %s' % tot_l
+print '\nDownloads of latest version: %s' % tot_l
 print 'Total downloads: %d' % tot
