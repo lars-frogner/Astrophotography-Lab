a519e02469a52f445804fbde194bff8dd849049f
Lars Frogner <lars_frogner@hotmail.com>
2017-03-30 09:44:21 +0200

    Update download counter

--- get_download_count.py
+++ get_download_count.py
@@ -1,8 +1,8 @@
 import requests
-starts = [0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 88, 63, 36]
+starts = [0, 0, 5, 0, 0, 29, 61, 0, 0, 0, 0, 0, 0, 0, 40, 0, 88, 63, 36]
 tot = 0
 tot_l = 0
-latest = [0, 1]
+latest = [0, 1, 2]
 i = 0
 print '\nIndividual download counts:'
 for release in requests.get('https://api.github.com/repos/lars-frogner/astrophotography-lab/releases').json():
@@ -15,7 +15,7 @@ for release in requests.get('https://api.github.com/repos/lars-frogner/astrophot
         tot += d
         if i in latest: tot_l += d
 
-        print '%-34s %d' % (release_assets['name'].replace('.zip', '').replace('.tar.gz', ''), d)
+        print '%-36s %d' % (release_assets['name'].replace('.zip', '').replace('.tar.gz', ''), d)
         
         i += 1
       
